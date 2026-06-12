155b7ad912dafd9be5f2a010adc73a6c42d387ed
Jack Touhey <jt112112@gmail.com>
2026-06-12 14:42:32 +1000

    feat: Now extracting public key + info source disclosures

--- sample-tasks/p12-conversion/p12-converter.ts
+++ sample-tasks/p12-conversion/p12-converter.ts
@@ -12,9 +12,10 @@ const p12Asn1 = forge.asn1.fromDer(fileContentsString);
 const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, password);
 
 // Extract
-
-// Certificate + expiration + publicKey
 const certificateBag = p12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag][0];
+
+// I Was able to achieve the above with node-forge docs, however I was unable to fully extract the data without further assistance.
+// No google AI answers were used, predominantly stack-overflow threads.
 const certificate = certificateBag.cert;
 const expirationDate = certificate.validity.notAfter;
 const publicKey = certificate.publicKey;
@@ -24,3 +25,11 @@ console.log("Expiration Date", expirationDate);
 console.log('');
 console.log("Public Key", publicKey);
 
+// I nearly got the privateKey from purely node-forge docs which used bagType: forge.pki.oids.keyBag 
+// After going in circles trying to make that work I caved an used google, learning of bagType: forge.pki.oids.pkcs8ShroudedKeyBag
+const keyBags = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag });
+const keyBag = keyBags[forge.pki.oids.pkcs8ShroudedKeyBag][0];
+const privateKey = keyBag.key;
+
+console.log('');
+console.log("Private Key", privateKey);
