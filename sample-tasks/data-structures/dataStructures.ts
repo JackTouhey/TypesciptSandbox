798cfe03d8802eebc1bace2cdc6b8f5ee2950576
Jack Touhey <jt112112@gmail.com>
2026-06-13 14:14:56 +1000

    chore: found how to extract keys from object. please ignore the mess :)

--- sample-tasks/data-structures/dataStructures.ts
+++ sample-tasks/data-structures/dataStructures.ts
@@ -1,16 +1,28 @@
 import pool from '../utils/getConnectionPool';
 
-const query = 'select owner.ownerid, owner.ownername, pet.petid, pet.petname, pet.ownerid from "owner" cross join pet where owner.ownerid = pet.ownerid';
+const query: string = 'select owner.ownerid, owner.ownername, pet.petid, pet.petname, pet.ownerid from "owner" cross join pet where owner.ownerid = pet.ownerid';
 
 
-async function runQuery(query: string) {
+async function runQuery(query: string): Promise<object[]> {
     try {
         const resultSet = await pool.query(query);
         console.log(resultSet.rows[0]);
+        console.log(Object.keys(resultSet.rows[0]));
+        return resultSet.rows;
     } catch (error) {
         console.error('Error executing query:', error);
         throw error;
     }
 }
 
-runQuery(query);
+const resultSet = runQuery(query);
+
+// console.log(resultSet[0]);
+
+function processData(resultSet: object, primaryKeyName: string | number) {
+    const keySet = Object.keys(resultSet);
+    console.log(keySet);
+}
+
+
+// processData(runQuery(query), 'pk');
