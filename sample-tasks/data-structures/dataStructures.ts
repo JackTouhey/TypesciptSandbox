39cc333d57e73fd81508725b8c4b5ebb10b8f738
Jack Touhey <jt112112@gmail.com>
2026-06-14 11:35:36 +1000

    feat: Now extracting tableAKeys

--- sample-tasks/data-structures/dataStructures.ts
+++ sample-tasks/data-structures/dataStructures.ts
@@ -1,11 +1,12 @@
 import pool from '../utils/getConnectionPool';
 
-const query: string = 'select owner.ownerid, owner.ownername, pet.petid, pet.petname, pet.ownerid from "owner" cross join pet where owner.ownerid = pet.ownerid';
+const sampleQuery: string = 'select owner.ownerid, owner.ownername, pet.petid, pet.petname, pet.ownerid from "owner" cross join pet where owner.ownerid = pet.ownerid';
+const primaryKeyName: string = 'ownerid';
 
-
-async function runQuery(query: string): Promise<object[]> {
+async function runQuery(query: string): Promise<Record<string, unknown>[]> {
     try {
         const resultSet = await pool.query(query);
+        console.log(resultSet.rows[0]);
         return resultSet.rows;
     } catch (error) {
         console.error('Error executing query:', error);
@@ -13,20 +14,46 @@ async function runQuery(query: string): Promise<object[]> {
     }
 }
 
-async function extractKeys(): Promise<string[]> {
-    const resultSet = await runQuery(query);
-    // Check if array exists and is not empty, then return keyset
-    return (!Array.isArray(resultSet) || !resultSet.length) ? [] : Object.keys(resultSet[0] as string[]); 
-}
+function processData(resultSet: Record<string, unknown>[], primaryKeyName: string) {
+    if (!resultSet.length) return;
 
-async function printKeys(){
-    console.log(await extractKeys())
-}
-
-async function processData(resultSet: object, primaryKeyName: string | number) {
-    const keySet = Object.keys(resultSet);
+    const keySet: string[] = extractKeys(resultSet[0] as Record<string, unknown>);
+    const tableAKeys: string[] = extractTableAKeys(resultSet as Record<string, unknown>[], keySet, primaryKeyName);
     console.log(keySet);
+    console.log(tableAKeys);
 }
 
+function extractKeys(resultSet: Record<string, unknown>): string[] {
+    return Object.keys(resultSet); 
+}
 
-printKeys();
+function extractTableAKeys(resultSet: Record<string, unknown>[], keySet: string[], primaryKeyName: string): string[] {
+    let tableAKeys: string[] = [];
+    for (let index = 1; index < resultSet.length; index++) {
+        const previousRow = resultSet[index - 1];
+        const currentRow = resultSet[index];
+
+        if (previousRow !== undefined && currentRow !== undefined) {
+            const previousPrimaryValue = previousRow[primaryKeyName];
+            const currentRowPrimaryValue = currentRow[primaryKeyName];
+
+            if (previousPrimaryValue === currentRowPrimaryValue) {
+                for (const key of keySet) {
+                    if (previousRow[key] === currentRow[key]) {
+                        tableAKeys.push(key);
+                    }
+                }
+                if (tableAKeys.length > 0) {
+                    break;
+                }
+            }       
+        }
+    }
+    return tableAKeys;
+}
+
+async function run() {
+    processData(await runQuery(sampleQuery), 'ownerid');
+}
+
+run();
