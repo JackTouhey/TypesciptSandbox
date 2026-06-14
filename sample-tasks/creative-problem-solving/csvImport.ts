bc333cf0fa557db2883b29e20dbca2442f06e1c8
Jack Touhey <jt112112@gmail.com>
2026-06-14 16:44:23 +1000

    feat: Now parsing csv into array of patients

--- sample-tasks/creative-problem-solving/csvImport.ts
+++ sample-tasks/creative-problem-solving/csvImport.ts
@@ -1,4 +1,4 @@
-import { parse } from 'csv-parse';
+import { parse } from 'csv-parse/sync';
 import * as fs from 'fs';
 
 type Patient = {
@@ -12,3 +12,24 @@ type Patient = {
     medicare?: number,
     ihi?: number
 }
+
+function parseCsvToPatients(): Patient[] {
+    const filePath = 'sample-tasks\\creative-problem-solving\\patients_import_sample.csv';
+    const headers = ['first_name', 'last_name', 'dob', 'gender', 'address', 'mobile', 'email', 'medicare_number', 'ihi'];
+    const fileContents = fs.readFileSync(filePath, { encoding: 'utf-8' });
+
+    let patientArray: Patient[] = [];
+
+    return parse(fileContents, {
+        delimiter: ',',
+        columns: headers,
+ 
+    });
+}
+
+function run() {
+    const patients = parseCsvToPatients();
+    console.log(patients);
+}
+
+run();
