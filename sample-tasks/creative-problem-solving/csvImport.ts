3e32279e5ff9eaf30f7472bceac6bdbc2ac20c69
Jack Touhey <jt112112@gmail.com>
2026-06-15 18:16:40 +1000

    feat: Added patient lookup

--- sample-tasks/creative-problem-solving/csvImport.ts
+++ sample-tasks/creative-problem-solving/csvImport.ts
@@ -1,10 +1,12 @@
 import { parse } from 'csv-parse/sync';
 import * as fs from 'fs';
+import pool from '../utils/getConnectionPool.ts';
+import { QueryResult } from 'pg';
 
 type Patient = {
     firstName?: string,
     lastName?: string,
-    dateOfBirth?: string,
+    dateOfBirth?: Date,
     gender?: string,
     address?: string,
     mobile?: number,
@@ -30,6 +32,56 @@ function parseCsvToPatients(): Patient[] {
 function run() {
     const patients = parseCsvToPatients();
     console.log(patients);
+
+    for(const patient of patients) {
+        processPatient(patient);
+        // Check if already in database
+    }
+
+}
+
+function processPatient(currentPatient: Patient) {
+    // Check if patient exists
+    
+}
+
+async function lookupPatient(currentPatient: Patient): Promise<Patient | undefined> {
+    // Check which identifier to use
+    return currentPatient.medicare !== undefined ? await medicareLookup(currentPatient) : await ihiLookup(currentPatient);
+}
+
+async function medicareLookup(currentPatient: Patient): Promise<Patient | undefined> {
+    if (currentPatient.medicare !== undefined) {
+        try {
+            const query = 'select first_name as firstName, last_name as lastName, dob as dateOfBirth, gender, address, mobile, email, medicare, ihi from patients where medicare = $1';
+            const value = [ currentPatient.medicare ]
+
+            const resultPatient: QueryResult<Patient> = await pool.query(query, value);
+            return resultPatient.rows[0];
+        } catch (error) {
+            console.log('Error performing medicareLookup: ', error);
+            return undefined;
+        }
+    } else {
+        return undefined;
+    }
+}
+
+async function ihiLookup(currentPatient: Patient): Promise<Patient | undefined> {
+    if (currentPatient.medicare !== undefined) {
+        try {
+            const query = 'select first_name as firstName, last_name as lastName, dob as dateOfBirth, gender, address, mobile, email, medicare, ihi from patients where ihi = $1';
+            const value = [ currentPatient.ihi ]
+
+            const resultPatient: QueryResult<Patient> = await pool.query(query, value);
+            return resultPatient.rows[0];
+        } catch (error) {
+            console.log('Error performing medicareLookup: ', error);
+            return undefined;
+        }
+    } else {
+        return undefined;
+    }
 }
 
 run();
