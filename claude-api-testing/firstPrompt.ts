5de89538237d7e05f5d9d8ca884b804e1095527d
Jack Touhey <jt112112@gmail.com>
2026-07-08 19:04:00 +1000

    feat: First programmatic access to claude

--- claude-api-testing/firstPrompt.ts
+++ claude-api-testing/firstPrompt.ts
@@ -1,11 +1,25 @@
 import * as dotenv from 'dotenv';
+import Anthropic from "@anthropic-ai/sdk";
 
 dotenv.config( { path: './config.env' });
 
 const apiKey = process.env.ANTHROPIC_API_KEY;
 
-if (apiKey) {
-    console.log('Key: ' + apiKey);
-} else {
-    console.log(':(');
-}
+const client = new Anthropic();
+
+async function submitPrompt(input: string) {
+    const response = await client.messages.create({
+        model: 'claude-haiku-4-5',
+        max_tokens: 1024,
+        messages: [
+            {
+                role: 'user',
+                content: input
+            }
+        ]
+    });
+
+    console.log(response);
+} 
+
+submitPrompt("This is a test to see if I now have programatic access to claude");
