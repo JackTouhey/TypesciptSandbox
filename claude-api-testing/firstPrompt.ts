0aefff095d4a77e33dc8bf4335d25785971d1ec3
Jack Touhey <jt112112@gmail.com>
2026-07-10 10:48:48 +1000

    feat: Now downloading generated file

--- claude-api-testing/firstPrompt.ts
+++ claude-api-testing/firstPrompt.ts
@@ -15,6 +15,7 @@ const downloadPath = process.env.DOWNLOAD_PATH !== undefined ? process.env.DOWNL
 const messages: Message[] = [];
 const fileIds: string[] = [
     process.env.CV_FILEID !== undefined ? process.env.CV_FILEID : "",
+    process.env.JOB_DESCRIPTION_ID !== undefined ? process.env.JOB_DESCRIPTION_ID : "",
     process.env.CL1 !== undefined ? process.env.CL1 : "",
     process.env.CL2 !== undefined ? process.env.CL2 : "",
     process.env.CL3 !== undefined ? process.env.CL3 : "", 
@@ -28,20 +29,34 @@ const fileIds: string[] = [
     process.env.CL11 !== undefined ? process.env.CL11 : "",
     process.env.CL12 !== undefined ? process.env.CL12 : "",
     process.env.CL13 !== undefined ? process.env.CL13 : "",
-    process.env.CL14 !== undefined ? process.env.CL14 : ""
+    process.env.CL14 !== undefined ? process.env.CL14 : "",
 ]
 
 
 async function submitPrompt(messages: Message[]) {
     const response = await client.beta.messages.create({
         model: 'claude-haiku-4-5',
-        max_tokens: 1024,
+        max_tokens: 5000,
         betas: ["code-execution-2025-08-25", "files-api-2025-04-14"],
         tools: [{ type: "code_execution_20250825", name: "code_execution" }],
         messages: messages
     });
 
     console.log(response);
+    console.log('------------------------------');
+
+    for (const block of response.content) {
+        if (block.type === 'bash_code_execution_tool_result') {
+            const codeExecutionContent = block.content;
+            if (codeExecutionContent.type === 'bash_code_execution_result') {
+                for (const generatedFile of codeExecutionContent.content) {
+                    const fileId = generatedFile.file_id;
+                    console.log("extracted fileId: " + fileId);
+                    return fileId;
+                }
+            }
+        }
+    }
 } 
 
 function createMessage(isUser: boolean, content: any): Message {
@@ -84,12 +99,14 @@ async function run() {
     fileIds.forEach((fileId: string) => {
         addFileToMessage(message, fileId);
     });
-    addFileToMessage(message, await uploadFile(uploadPath));
+    // addFileToMessage(message, await uploadFile(uploadPath));
 
     messages.push(message);
-    const response = submitPrompt(messages);
+    const generatedFileId = await submitPrompt(messages);
 
-    
+    if (generatedFileId !== undefined) {
+        downloadFile(generatedFileId);
+    }
 }
 
 run();
